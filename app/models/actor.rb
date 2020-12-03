8866ddb7f2b03d5d611216dfba87cf457311f193
AlexanderBowers <bowers@MSI.localdomain>
2020-12-03 14:34:44 -0800

    finished lab

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Actor < ActiveRecord::Base
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        roles = []
+        Show.all.map do |show| 
+            show.characters.map do |character|
+                if character.actor_id == self.id
+                  roles << "#{character.name} - #{show.name}"
+                end
+            end
+        end
+        roles
+    end
   
 end
